3ee4041b4097403cc974a62dd8bb5258ae6ea123
bys1r22 <bys1r22@soton.ac.uk>
2022-07-02 21:26:57 +0800

    post vs get

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("form", views.form, name="form"),
+    path("counter", views.counter, name="counter")
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hey, Welcome</h1>")
+    # sending dynamic data(like from db)
+    context = {
+        "name": "Patrick",
+        "age": 23,
+        "nationality": "Taiwanese"
+    }
+    return render(request, "index.html", context)
+
+def form(request):
+
+    return render(request, "form.html")
+
+@csrf_exempt
+def counter(request):
+    words = request.POST['words']
+    amount_of_words = len(words.split())
+    return render(request, "counter.html", {"amount": amount_of_words})
